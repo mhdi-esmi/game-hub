da20e0c1165ab783a6d0a1eef67c55e9a0215ed2
mhdi <m.esmaeili@gmail.com>
2024-02-09 19:44:41 +0330

    crop image url

--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -19,19 +19,22 @@ export interface Platform{
     results: Game[];
   }
 const useGames=()=>{
-    const controller=new AbortController;
     const [games, setGame] = useState<Game[]>([]);
-    const [error, setError] = useState("");
+    const [error, setError] = useState('');
     useEffect(() => {
+      const controller=new AbortController();
       apiClient
         .get<FetchGameresponse>("/games",{signal:controller.signal})
         .then((res) => setGame(res.data.results))
         .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError(err.message)});
+            if (err instanceof CanceledError)  return;
+            setError(err.message)
+          });
 
         return ()=>controller.abort()
-    },[]);
+    },[error]);
+    // console.log(games);
+    
     return {games,error}
 }
 export default useGames;
