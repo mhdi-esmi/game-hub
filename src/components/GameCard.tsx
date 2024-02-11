dd31a7d30bf1e33e6fbe73519c96bfaeaf4bc3f1
mhdi <m.esmaeili@gmail.com>
2024-02-11 19:40:13 +0330

    Cleaning gameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -17,21 +17,21 @@ interface Props {
   game: Game;
 }
 
-const CardGame = ({ game }: Props) => {
+const GameCard = ({ game }: Props) => {
   return (
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="1xl">{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
+        <HStack justifyContent={"space-between"} marginBottom={2}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="1xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
 };
 
-export default CardGame;
+export default GameCard;
