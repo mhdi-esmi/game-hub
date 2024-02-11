2ccfc8deb1b6682cbc7f5bf0470ee9ab76b87fc1
mhdi <m.esmaeili@gmail.com>
2024-02-11 21:08:37 +0330

    Customixe the theme to darker

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,25 @@ import { extendTheme,ThemeConfig } from "@chakra-ui/react";
 const config:ThemeConfig={
     initialColorMode:'dark'
 }
-const theme=extendTheme({config})
+const theme=extendTheme({
+    config,
+    colors:{
+        gray:{
+           50: '#bfbfbf',
+           100: '#d9d9d9',
+           200: '#bfbfbf',
+           300: '#a6a6a6',
+           400: '#7095a7',
+           500: '#737373',
+           600: '#595959',
+           700: '#404040',
+           800: '#262626',
+           900: '#0d0d0d',
+        },
+        White:{
+            50: '#bfbfbf',
+            100: '#d9d9d9',
+        }
+    }
+})
 export default theme;
